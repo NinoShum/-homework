e8d46cc98126af2b63acd1ccbf0c552ff5fd043e
Шумайлова Нина <ninashum@gmail.com>
2022-10-14 02:40:35 +0300

    ex_2

---
+++ homework_lesson_1/ex_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход
+ три числа и выдаёт максимальное из этих чисел. */
+
+ // Ввод чисел
+Console.Write("Введите X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Z: ");
+int Z = Convert.ToInt32(Console.ReadLine());
+
+// ввод максимума
+
+int max = X;
+
+//проверка условия
+
+if ( Y > max) max = Y;
+if ( Z > max) max = Z;
+
+//ответ
+Console.WriteLine("Максимальное число равно " + max);
