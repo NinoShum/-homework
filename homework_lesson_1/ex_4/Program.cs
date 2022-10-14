de3331f5022734dab0bb6010dda6f523f5bfc14c
Шумайлова Нина <ninashum@gmail.com>
2022-10-14 03:25:48 +0300

    ex_4

---
+++ homework_lesson_1/ex_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// доп.переменная - первое четное число
+int count = 2;
+
+if (number >= count)  //даное условие предполагает,что если N<2, то нет чисел для поиска
+{
+    while (count <= number) 
+    {
+        Console.Write(count + " ");
+        count = count + 2;
+    }
+} 
+else
+{
+    Console.WriteLine($"До числа {number} нет положительных четных чисел");
+}
