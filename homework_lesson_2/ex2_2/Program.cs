9bf8f1a2b33666ae1b37c7aaaae66821c1745c87
Шумайлова Нина <ninashum@gmail.com>
2022-10-18 01:00:50 +0300

    ex2_2

---
+++ homework_lesson_2/ex2_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа
+ или сообщает, что третьей цифры нет. */
+
+// ввод числа 
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//проверка числа
+if ((number > 99) || (number < -99))
+{
+    if ((number > 1000) || (number < -1000))
+    {
+        while ((number > 1000) || (number < -1000)) //если больше трех знаков
+        {
+            number = number / 10 ;
+        }
+    }
+    int result = number % 10;
+    Console.WriteLine($"Третья цифра {result}");
+
+}
+else Console.WriteLine("Третьей цифры нет");
+
