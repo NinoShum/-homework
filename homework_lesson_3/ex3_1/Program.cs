403d91e3255c90d7cfe625ecf46689bb3f2ccfc5
Шумайлова Нина <ninashum@gmail.com>
2022-10-21 02:51:17 +0300

    ex3_1

---
+++ homework_lesson_3/ex3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+
+void testN (int N)
+{
+    if (N > 9999 && N < 100000)
+    {
+        Console.WriteLine ("Число состоит из 5 знаков");
+        int Temp  = N;
+        int Revers = 0;
+        int Rest = 0;
+        while (Temp > 0)
+        {
+            Rest = Temp % 10;
+            Revers = Revers * 10 + Rest;
+            Temp = Temp / 10;
+        }
+         Console.WriteLine (Revers);
+        if (N == Revers) 
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        Console.WriteLine("Число НЕ является палиндромом");
+        
+
+    
+    }
+    else Console.WriteLine ("число НЕ состоит из 5 знаков или содержит знак минус. Введите нужное число");
+}
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+testN(N);
