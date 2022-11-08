0daea568d4825a4526e780e14b7b16ab94643b0c
Шумайлова Нина <ninashum@gmail.com>
2022-11-09 00:12:28 +0300

    ex4_3

---
+++ homework_lesson_4/ex4_3/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая задаёт массив 
+из 8 элементов и выводит их на экран.
+Заполнение массива производит пользователь. */
+
+ void GetArray (int [] array) 
+ {
+    for (int i = 0; i < array.Length ; i++)
+    {
+        Console.Write($"Введите число с индексом {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.Write("[{0}]", string.Join(", ", array));
+    
+ }
+
+int [] array = new int [8];
+GetArray(array);
