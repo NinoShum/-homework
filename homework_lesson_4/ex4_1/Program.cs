d8e1d47fcd2d911d8f41543ea1da524ceae54d79
Шумайлова Нина <ninashum@gmail.com>
2022-11-08 22:46:45 +0300

    ex41

---
+++ homework_lesson_4/ex4_1/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Напишите программу, которая принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B. Для решения задания использование цикла for является обязательным условием. 
+Не использовать встроенный метод возведения в степень.*/
+
+void Calculation( int number, int N)
+{
+    if (N > 0)
+    {
+        int result = 1;
+        for (int i = 1; i <= N; i++)
+        {
+            result = result * number;
+        }
+        Console.WriteLine($"Число в степени равно: {result}");
+
+    }
+    if (N == 0) 
+    {
+            int result = 1;
+            result = 1 * 1;
+            Console.WriteLine($"Число в степени равно: {result}");
+
+    }
+    if (N < 0)
+    {
+        int N1 = Math.Abs(N);
+        int result = 1;
+        for (int i = 1; i <= N1; i++)
+        {
+            result = (1 / (result * number));
+        }
+        Console.WriteLine($"Число в степени равно: {result}");
+
+    }
+
+}
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Calculation(number, N);
+
