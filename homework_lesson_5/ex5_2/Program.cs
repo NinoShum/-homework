8ab32cc2ca90e26d92bc1428899a66d52b8313e2
Шумайлова Нина <ninashum@gmail.com>
2022-11-12 03:57:52 +0300

    ex5_2

---
+++ homework_lesson_5/ex5_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+
+int [] array = new int [ new Random().Next(4, 10)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i] + " ");
+}
+
+int Sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 == 1)
+    {
+        Sum += array[i];
+    }
+}
+Console.WriteLine(" ");
+Console.WriteLine($"Сумма цифр нечетных позиций равно: {Sum}");
