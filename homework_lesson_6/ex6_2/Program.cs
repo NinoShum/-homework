fe683fa91c90afc7baae8b2a3d419895c8b3d884
Шумайлова Нина <ninashum@gmail.com>
2022-11-16 22:01:54 +0300

    ex6-2

---
+++ homework_lesson_6/ex6_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+*/
+
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if ((k1==k2) && (b1==b2)) {Console.WriteLine("это одна и таже прямая");}
+else if ((k1==k2) && (b1!=b2)) {Console.WriteLine("это параллельные прямые");}
+else 
+{
+    double x = ( (b2-b1)/(k1-k2) );
+    double y = ( k1 * x + b1 );
+    Console.WriteLine($"точка пересечения прямых ({x};{y})");
+}
+
+
