1318c40bca77da89415ea617d7835568d2cec869
Шумайлова Нина <ninashum@gmail.com>
2022-11-16 21:21:45 +0300

    ex6-1

---
+++ homework_lesson_6/ex6_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+void CountPositiveNumber (int N)
+{
+    if (N > 0)
+    {
+        int Count = 0;
+        for (int i = 0; i < N; i++ )
+        {
+            Console.WriteLine("Введите число: ");
+            int numeral = Convert.ToInt32(Console.ReadLine());
+            if ( numeral > 0 )
+            {
+                Count = Count + 1;
+            }
+        }
+        Console.WriteLine($"Количество положительных чисел:{Count}");
+
+    }
+    else Console.WriteLine("Введите положительное число");
+}
+Console.WriteLine("Введите количество цифр: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+CountPositiveNumber(number);
