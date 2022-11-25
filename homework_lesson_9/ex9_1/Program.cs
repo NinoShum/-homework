7ed7b1571be8858ef9bba0ad91abb1ef70066d0f
Шумайлова Нина <ninashum@gmail.com>
2022-11-26 01:43:21 +0300

    ex8_5

---
+++ homework_lesson_9/ex9_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет 
+все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.*/
+
+int Formula(int n)
+
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    Console.Write (n+", ");
+    return Formula(n-1);
+}
+
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n <= 0)  
+Console.WriteLine("Error");
+else
+{
+int result = Formula(n);
+Console.WriteLine(result);}
